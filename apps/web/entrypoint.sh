ccc57591ff32528bfb1f9ff23f0ccbebf0304548
hi-hyunjung <nhannhathegreat@gmail.com>
2025-04-16 10:27:51 +0900

    [Feature: next port change] * port 환경변수 주입

--- apps/web/entrypoint.sh
+++ apps/web/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 echo "NEXT_PUBLIC_API_BASE_URL: $NEXT_PUBLIC_API_BASE_URL"
 echo "NEXT_PUBLIC_MAX_DAYS: $NEXT_PUBLIC_MAX_DAYS"
+echo "PORT: $PORT"
 
 echo "Check that we have NEXT_PUBLIC_API_BASE_URL and NEXT_PUBLIC_MAX_DAYS vars"
 
 test -n "$NEXT_PUBLIC_API_BASE_URL"
 test -n "$NEXT_PUBLIC_MAX_DAYS"
+test -n "$PORT"
 
 find /app/apps/web/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_API_BASE_URL#$NEXT_PUBLIC_API_BASE_URL#g"
 find /app/apps/web/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_MAX_DAYS#$NEXT_PUBLIC_MAX_DAYS#g"
+find /app/apps/web/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#PORT#$PORT#g"
 
 echo "Starting Nextjs"
 exec "$@"
