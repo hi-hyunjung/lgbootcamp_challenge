359596a0cc4780478ee290f3575eac5c1b30a460
서현명 <gusaud2199@gmail.com>
2025-04-17 16:29:11 +0900

    [web] 에러 로그 확인을 위한 옵션 추가
    :Detailed Notes:
    - NODE_OPTIONS
    - reactStrictMode
    - compiler.removeConsole
    - productionBrowserSourceMaps

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,16 +7,19 @@ createJiti(fileURLToPath(import.meta.url))('./src/env');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: process.env.NODE_ENV === 'production',
+  // reactStrictMode: process.env.NODE_ENV === 'production',
+  reactStrictMode: false,
   i18n: i18nConfig.default.i18n,
   output: 'standalone',
   eslint: { ignoreDuringBuilds: true },
   transpilePackages: ['@ufb/react'],
-  compiler: { removeConsole: process.env.NODE_ENV === 'production' },
+  // compiler: { removeConsole: process.env.NODE_ENV === 'production' },
+  compiler: { removeConsole: false },
   images: { remotePatterns: [{ hostname: '*' }], path: '/lgweb/_next/image' },
   basePath: '/lgweb',
   assetPrefix: '/lgweb',
   trailingSlash: true,
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
