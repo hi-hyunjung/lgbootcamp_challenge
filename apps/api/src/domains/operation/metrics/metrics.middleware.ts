12aa71bb30b0b0a6a0709418c7aa38adc23d4691
Dongho Lee <ghehd1125@gmail.com>
2025-04-21 11:10:00 +0900

    add prometheus api

---
+++ apps/api/src/domains/operation/metrics/metrics.middleware.ts
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2025 LY Corporation
+ *
+ * LY Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+import { httpRequestsTotal } from './metrics.counter';
+
+@Injectable()
+export class MetricsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.on('finish', () => {
+      const method = req.method;
+      // const path = req.route?.path || req.path;
+      const path = req.originalUrl;
+      const status = res.statusCode.toString();
+
+      httpRequestsTotal.labels(method, path, status).inc();
+    });
+
+    next();
+  }
+}
